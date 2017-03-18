1ea87b7f85ad6b86b18cbe1ce19a1189e359208b
Alexey Groshev <aleksejs.grosevs@gmail.com>
2017-03-18 16:48:09 +0000

    Roman to arabic conversion.

--- TheRomanNumeralKata.Domain.Tests/RomanNumberTests.cs
+++ TheRomanNumeralKata.Domain.Tests/RomanNumberTests.cs
@@ -5,16 +5,30 @@ namespace TheRomanNumeralKata.Domain.Tests
     [TestFixture]
     public class RomanNumberTests
     {
-        [TestCase(0, "N")]
         [TestCase(1, "I")]
         [TestCase(10, "X")]
         [TestCase(1100, "MC")]
         [TestCase(1990, "MCMXC")]
         [TestCase(2008, "MMVIII")]
+        [TestCase(1954, "MCMLIV")]
+        [TestCase(2014, "MMXIV")]
         public void Should_convert_arabic_to_roman_numbers_correctly(int number, string result)
         {
             var rn = new RomanNumber(number);
             Assert.AreEqual(result, rn.ToString());
+        }
+
+        [TestCase("I", 1)]
+        [TestCase("X", 10)]
+        [TestCase("MC", 1100)]
+        [TestCase("MCMXC", 1990)]
+        [TestCase("MMVIII", 2008)]
+        [TestCase("MCMLIV", 1954)]
+        [TestCase("MMXIV", 2014)]
+        public void Should_convert_roman_numbers_to_arabic_correctly(string number, int result)
+        {
+            var rn = new RomanNumber(number);
+            Assert.AreEqual(result, (int)rn);
         }
     }
 }
--- TheRomanNumeralKata.Domain/RomanNumber.cs
+++ TheRomanNumeralKata.Domain/RomanNumber.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace TheRomanNumeralKata.Domain
 {
@@ -9,11 +11,27 @@ namespace TheRomanNumeralKata.Domain
         private readonly string[] _hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         private readonly string[] _tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         private readonly string[] _ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        private readonly Dictionary<char, int> _romanSymbols = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
 
         public RomanNumber(int number)
         {
             _number = number;
             _value = Convert(_number);
+        }
+
+        public RomanNumber(string value)
+        {
+            _number = Convert(value);
+            _value = value;
         }
 
         public static implicit operator int(RomanNumber rn)
@@ -28,8 +46,8 @@ namespace TheRomanNumeralKata.Domain
 
         private string Convert(int number)
         {
-            if (_number == 0)
-                return "N";
+            if (number <= 0)
+                throw new InvalidOperationException("Only positive values allowed");
 
             var sb = new StringBuilder();
 
@@ -47,6 +65,37 @@ namespace TheRomanNumeralKata.Domain
 
             sb.Append(_ones[number]);
             return sb.ToString();
+        }
+
+        private int Convert(string romanNumber)
+        {
+            if (string.IsNullOrEmpty(romanNumber))
+                throw new InvalidOperationException("Empty string is not valid Roman number");
+
+            var result = 0;
+            var prevNum = 0;
+
+            for (var i = romanNumber.Length - 1; i >= 0; i--)
+            {
+                if (_romanSymbols.ContainsKey(romanNumber[i]))
+                {
+                    if (prevNum > _romanSymbols[romanNumber[i]])
+                    {
+                        result -= _romanSymbols[romanNumber[i]];
+                    }
+                    else
+                    {
+                        result += _romanSymbols[romanNumber[i]];
+                    }
+                    prevNum = _romanSymbols[romanNumber[i]];
+                }
+                else
+                {
+                    throw new InvalidOperationException($"{romanNumber[i]} is not valid symbol");
+                }
+            }
+
+            return result;
         }
     }
 }
